741f9c4709b1984dcca3dcd29d0f109ef985296a
Lysender <lysender@darkstar-win>
2025-05-22 12:38:39 +0800

    fix: edit photos controls

--- website/frontend/public/assets/js/photo-gallery.js
+++ website/frontend/public/assets/js/photo-gallery.js
@@ -2,31 +2,6 @@
   if (!window.X_GALLERY_EVENTS) {
     window.X_GALLERY_EVENTS = true;
 
-    function handleToggleAlbumMenu() {
-      const container = document.getElementById('btn-album-menu');
-      if (container) {
-        container.classList.toggle('is-active');
-      }
-    }
-
-    function togglePhotosMenu() {
-      const container = document.getElementById('photo-gallery');
-      if (container) {
-        container.classList.toggle('photo-grid-edit');
-      }
-
-      const menu = document.getElementById('menu-edit-photos');
-      if (menu) {
-        menu.classList.toggle('is-active');
-      }
-
-      // Also toggle the trigger button color
-      const btn = document.getElementById('btn-album-menu-trigger');
-      if (btn) {
-        btn.classList.toggle('is-info');
-      }
-    }
-
     function handlePhotoDeleted() {
       const currentNode = document.querySelector(
         '#photos-count-w .current-count',
@@ -62,18 +37,6 @@
       });
 
       lightbox.init();
-    });
-
-    document.addEventListener('click', (e) => {
-      if (e.target.closest('#menu-edit-photos')) {
-        togglePhotosMenu();
-      }
-    });
-
-    document.addEventListener('click', (e) => {
-      if (e.target.closest('#btn-album-menu')) {
-        handleToggleAlbumMenu();
-      }
     });
 
     htmx.on('PhotoDeletedEvent', handlePhotoDeleted);
