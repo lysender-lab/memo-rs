d039a6692acc6112d5d9d626af8f5430b8a1bdbe
Lysender <lysender@darkstar>
2026-04-11 22:05:27 +0800

    fix: migration

--- db2/db/migrations/02-create-clients.sql
+++ db2/db/migrations/02-create-clients.sql
@@ -4,7 +4,7 @@ CREATE TABLE clients (
     status TEXT NOT NULL,
     default_bucket_id TEXT NULL DEFAULT NULL,
     admin INTEGER NULL DEFAULT 0,
-    created_at INTEGER NOT NULL,
+    created_at INTEGER NOT NULL
 ) STRICT;
 
 CREATE UNIQUE INDEX idx_clients_name ON clients(name);
