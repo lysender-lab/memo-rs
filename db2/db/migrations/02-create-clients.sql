a302224ba44f01ac0f2aaa9256907ef1d580fac9
Lysender <lysender@darkstar>
2026-04-11 14:35:11 +0800

    fix: good stuff

--- db2/db/migrations/02-create-clients.sql
+++ db2/db/migrations/02-create-clients.sql
@@ -5,8 +5,6 @@ CREATE TABLE clients (
     default_bucket_id TEXT NULL DEFAULT NULL,
     admin INTEGER NULL DEFAULT 0,
     created_at INTEGER NOT NULL,
-    updated_at INTEGER NULL DEFAULT NULL,
-    deleted_at INTEGER NULL DEFAULT NULL
 ) STRICT;
 
-CREATE UNIQUE INDEX idx_clients_name_deleted_at ON clients(name, deleted_at);
+CREATE UNIQUE INDEX idx_clients_name ON clients(name);
