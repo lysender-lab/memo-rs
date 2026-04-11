c72444c88f14d620859e4094d29aa3c6698d00d4
Lysender <lysender@darkstar>
2026-04-11 16:15:31 +0800

    fix: add files migration

---
+++ db2/db/migrations/06-create-files.sql
@@ -0,0 +1,18 @@
+CREATE TABLE files (
+    id TEXT PRIMARY KEY,
+    dir_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    filename TEXT NOT NULL,
+    content_type TEXT NOT NULL,
+    size INTEGER NOT NULL,
+    is_image INTEGER NOT NULL,
+    img_dimension TEXT NOT NULL,
+    img_versions TEXT NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL,
+    FOREIGN KEY (dir_id) REFERENCES dirs(id)
+) STRICT;
+
+CREATE INDEX idx_files_dir_id ON files(dir_id);
+CREATE UNIQUE INDEX idx_files_dir_id_name ON files(dir_id, name);
+CREATE INDEX idx_files_dir_id_created_at ON files(dir_id, created_at);
