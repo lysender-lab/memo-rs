91c9f5f3127c3a552c988b27a4143ad711714af6
Lysender <lysender@darkstar>
2026-05-24 11:09:34 +0800

    fix: switch to aws

--- storage/src/provider.rs
+++ storage/src/provider.rs
@@ -4,7 +4,7 @@ use memo::dir::DirMeta;
 use memo::file::FileDto;
 
 use crate::Result;
-use crate::providers::GoogleStorageProvider;
+use crate::providers::{AwsStorageProvider, GoogleStorageProvider};
 
 #[derive(Debug, Clone)]
 pub struct DownloadedFile {
@@ -38,42 +38,49 @@ pub struct UploadUrlRequest<'a> {
 
 pub enum ProviderClient {
     Google(GoogleStorageProvider),
+    Aws(AwsStorageProvider),
 }
 
 impl ProviderClient {
     pub async fn upload(&self, dir: &DirMeta, source_dir: &Path, file: &FileDto) -> Result<()> {
         match self {
             Self::Google(provider) => provider.upload(dir, source_dir, file).await,
+            Self::Aws(provider) => provider.upload(dir, source_dir, file).await,
         }
     }
 
     pub async fn download(&self, req: DownloadRequest<'_>) -> Result<DownloadedFile> {
         match self {
             Self::Google(provider) => provider.download(req).await,
+            Self::Aws(provider) => provider.download(req).await,
         }
     }
 
     pub async fn delete(&self, dir: &DirMeta, file: &FileDto) -> Result<()> {
         match self {
             Self::Google(provider) => provider.delete(dir, file).await,
+            Self::Aws(provider) => provider.delete(dir, file).await,
         }
     }
 
     pub async fn attach_urls(&self, dir: &DirMeta, files: Vec<FileDto>) -> Result<Vec<FileDto>> {
         match self {
             Self::Google(provider) => provider.attach_urls(dir, files).await,
+            Self::Aws(provider) => provider.attach_urls(dir, files).await,
         }
     }
 
     pub async fn attach_url(&self, dir: &DirMeta, file: FileDto) -> Result<FileDto> {
         match self {
             Self::Google(provider) => provider.attach_url(dir, file).await,
+            Self::Aws(provider) => provider.attach_url(dir, file).await,
         }
     }
 
     pub async fn generate_upload_url(&self, req: UploadUrlRequest<'_>) -> Result<String> {
         match self {
             Self::Google(provider) => provider.generate_upload_url(req).await,
+            Self::Aws(provider) => provider.generate_upload_url(req).await,
         }
     }
 }
